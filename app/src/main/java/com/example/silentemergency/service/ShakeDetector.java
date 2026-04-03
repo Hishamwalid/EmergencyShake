cf65304a2b4e2193adca0a563294e9e7b435839a
afif020905 <mutasim.afif09@gmail.com>
2026-04-03 10:53:17 +0600

    service updata to shake detector

--- app/src/main/java/com/example/silentemergency/service/ShakeDetector.java
+++ app/src/main/java/com/example/silentemergency/service/ShakeDetector.java
@@ -1,1 +1,10 @@
 package com.example.silentemergency.service;
+
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+
+public class ShakeDetector implements SensorEventListener {
+    @Override public void onSensorChanged(SensorEvent event) { }
+    @Override public void onAccuracyChanged(Sensor sensor, int accuracy) { }
+}
