59fd96f55ce64703f4f4b064ad8fa2ed3905dac8
afif020905 <mutasim.afif09@gmail.com>
2026-04-03 10:59:24 +0600

    helper updata to locationhelper

--- app/src/main/java/com/example/silentemergency/helper/LocationHelper.java
+++ app/src/main/java/com/example/silentemergency/helper/LocationHelper.java
@@ -1,1 +1,8 @@
 package com.example.silentemergency.helper;
+
+import android.content.Context;
+
+public class LocationHelper {
+    public LocationHelper(Context context) { }
+    public void sendLocationAndAlert(String phoneNumber) { }
+}
