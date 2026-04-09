b50cf07529702b926412ee47c1d94cf61d8e05f2
Hishamwalid <hishamwalid666555@gmail.com>
2026-04-09 13:20:32 +0600

    fixed settings activity

--- app/src/main/java/com/example/silentemergency/SettingsActivity.java
+++ app/src/main/java/com/example/silentemergency/SettingsActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.*;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import com.example.silentemergency.service.EmergencyService;
